d050243d2b0be0de178a1b2d2afc4416eab56c3d
sandermaasikamae <147600257+sandermaasikamae@users.noreply.github.com>
2023-10-16 07:32:24 +0000

    added fileanalyse functionality

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const upload = multer();
+
 var app = express();
 
 app.use(cors());
@@ -11,7 +14,9 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res) {
+  res.json({name: req.file.originalname, type: req.file.mimetype, size: req.file.size});
+});
 
 
 const port = process.env.PORT || 3000;
